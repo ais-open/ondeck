9e973d90ac8743b481483781079f14ea95711a4b
Michael Bowman <bowmanmc@gmail.com>
2017-05-23 10:36:31 -0400

    settings modal hooked up to keyboard shortcut

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Shortcuts, ShortcutManager } from 'react-shortcuts';
+import ReactModal from 'react-modal';
 
 import MapComponent from '../components/map';
 import Keymap from './HomePageKeymap';
@@ -11,9 +12,13 @@ class HomePage extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            isModalOpen: false
+        };
+
         this.shortcutManager = new ShortcutManager(Keymap);
 
-        this.handleShortcuts = this.handleShortcuts.bind(this);
+        this.setModalState = this.setModalState.bind(this);
     }
 
     getChildContext() {
@@ -25,14 +30,32 @@ class HomePage extends React.Component {
     componentWillMount() {
     }
 
-    handleShortcuts(action, evt) {
-        console.log('Got shortcut: ' + action);
+    setModalState(action) {
+        if (action === 'OPEN') {
+            this.setState({
+                isModalOpen: true
+            });
+        }
+        else {
+            this.setState({
+                isModalOpen: false
+            });
+        }
     }
 
     render() {
         return (
-            <Shortcuts name='SETTINGS' handler={this.handleShortcuts}>
+            <Shortcuts name='SETTINGS' handler={this.setModalState}>
                 <MapComponent />
+                <ReactModal isOpen={this.state.isModalOpen}
+                    style={{
+                        overlay: {
+                            backgroundColor: 'rgba(0, 0, 0, 0.75)'
+                        }
+                    }}
+                    contentLabel="On Deck Settings">
+                    <button onClick={this.setModalState}>Close</button>
+                </ReactModal>
             </Shortcuts>
         );
     }
@@ -40,6 +63,6 @@ class HomePage extends React.Component {
 
 HomePage.childContextTypes = {
   shortcuts: PropTypes.object.isRequired
-}
+};
 
 export default HomePage;
