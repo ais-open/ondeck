f62b6c6032253c1422a2ff7994918a48587ad456
Michael Bowman <bowmanmc@gmail.com>
2017-09-12 16:32:21 -0400

    load state from url if it is there

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Shortcuts, ShortcutManager } from 'react-shortcuts';
+import QueryString from 'query-string';
 
 import ConfigurationManager from '../config';
 import MapComponent from '../components/map';
@@ -27,6 +28,27 @@ class HomePage extends React.Component {
         this.setSettingsPaneVisibility = this.setSettingsPaneVisibility.bind(this);
         this.resetConfiguration = this.resetConfiguration.bind(this);
         this.updateConfiguration = this.updateConfiguration.bind(this);
+
+        this.checkStateful();
+    }
+
+    checkStateful() {
+        const stateId = QueryString.parse(location.search).state;
+        if (!stateId) {
+            return;
+        }
+
+        const page = this;
+        // Load it from stateful...
+        const statefulUrl = `${APP_CONFIG.stateful}/states/state/${stateId}`;
+        fetch(statefulUrl, {cors: true}).then(response => {
+            return response.json();
+        }).then(config => {
+            //console.log('Got configuration ' + JSON.stringify(config) + ' from stateful');
+            page.setState({
+                configuration: JSON.parse(config.user_state)
+            });
+        });
     }
 
     getChildContext() {
